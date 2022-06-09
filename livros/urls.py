b1d5bf0aa9e2e9b4b348f04582d66cfeb2c82d37
Marcielle de Paula <marcielle0644@gmail.com>
2022-06-08 22:23:29 -0300

    Funcionalidade de Editar livros

--- livros/urls.py
+++ livros/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import listarLivros, inserirLivro
 from .views import listarLivros, inserirLivro, atualizarLivro, deletarLivro
 
 urlpatterns = [
     path("", listarLivros, name="listar-livros"),
     path("create", inserirLivro, name="adicionar-livro"),
+    path("edit/<int:id>", atualizarLivro, name="editar-livro"),
     path("delete/<int:id>", deletarLivro, name="deletar-livro"),
 ]
--- livros/views.py
+++ livros/views.py
@@ -15,6 +15,14 @@ def inserirLivro(request):
         return redirect('listar-livros')
     return render(request, 'criar-livro.html', {'form': form})
 
+def atualizarLivro(request, id):
+    livro = Livro.objects.get(id=id)
+    form = LivroForm(request.POST or None, request.FILES or None, instance=livro)
+    if form.is_valid():
+        form.save()
+        return redirect('listar-livros')
+    return render(request, 'editar-livro.html', {'form': form, 'livro': livro})
+
 def deletarLivro(request, id):
     livro = Livro.objects.get(id=id)
     if request.method == 'POST':
