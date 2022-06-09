fd09e13b7789dfff81eeb1331e473487d658fefe
Marcielle de Paula <marcielle0644@gmail.com>
2022-06-08 22:17:55 -0300

    Adicionando validações de data e nome no modelo e form

--- livros/forms.py
+++ livros/forms.py
@@ -1,25 +1,25 @@
 from dataclasses import fields
-from django import forms 
+from django import forms
 from .models import Livro
+from datetime import date
 
 class LivroForm(forms.ModelForm):
     class Meta:
         model = Livro
         fields = ['name', 'category', 'book_cover', 'author', 'publication_date', 'pages']
     
-    def clean_name(self):
-        name = self.cleaned_data.get('name')
-        livro = Livro.objects.filter(name=name).count()
-        if livro:
-            raise forms.ValidationError("Já existe um livro cadastrado com esse nome!")
-        return name
-
     def clean_pages(self):
         pages = self.cleaned_data.get('pages')
         if pages <= 0:
             raise forms.ValidationError("Insira uma quantidade válida de paginas!")
         return pages
     
-
-        
-    
+    def clean_publication_date(self):
+        ano1500 = date.fromisoformat('1500-01-01')
+        ano2022 = date.fromisoformat('2022-12-31')
+        publication_date = self.cleaned_data.get('publication_date')
+        publication_dateFinal = date.fromisoformat(str(publication_date))
+        if ano1500 <= publication_dateFinal <= ano2022:
+            return publication_date
+        else:
+            raise forms.ValidationError("A data de publicação precisa estar entre 1500 e 2022!")
--- livros/models.py
+++ livros/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Livro(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     category = models.CharField(max_length=255)
     book_cover = models.ImageField(upload_to='images')
     author = models.CharField(max_length=255)
@@ -9,3 +9,7 @@ class Livro(models.Model):
     pages = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
